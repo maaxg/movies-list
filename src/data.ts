273a4763f23af9dbd658637224b27a05350c6d9b
maaxg <maxgps5@hotmail.com>
2023-11-04 15:43:48 -0300

    feat: movies search and nominations

---
+++ src/data.ts
@@ -0,0 +1,127 @@
+export const data = [
+  {
+    id: 1,
+    synopsis: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+    image: "https://example.com/image1.jpg",
+    title: "Movie 1",
+  },
+  {
+    id: 2,
+    synopsis:
+      "Suspendisse cursus justo non erat vehicula, nec luctus elit facilisis.",
+    image: "https://example.com/image2.jpg",
+    title: "Movie 2",
+  },
+  {
+    id: 3,
+    synopsis: "Nullam vehicula arcu in tincidunt efficitur.",
+    image: "https://example.com/image3.jpg",
+    title: "Movie 3",
+  },
+  {
+    id: 4,
+    synopsis: "Vestibulum tincidunt risus sit amet congue hendrerit.",
+    image: "https://example.com/image4.jpg",
+    title: "Movie 4",
+  },
+  {
+    id: 5,
+    synopsis:
+      "Fusce consectetur ligula non dolor auctor, at bibendum libero tristique.",
+    image: "https://example.com/image5.jpg",
+    title: "Movie 5",
+  },
+  {
+    id: 6,
+    synopsis: "Proin vitae leo id erat tempor facilisis.",
+    image: "https://example.com/image6.jpg",
+    title: "Movie 6",
+  },
+  {
+    id: 7,
+    synopsis: "Curabitur id metus a lorem euismod auctor.",
+    image: "https://example.com/image7.jpg",
+    title: "Movie 7",
+  },
+  {
+    id: 8,
+    synopsis: "Mauris at velit nec turpis posuere egestas.",
+    image: "https://example.com/image8.jpg",
+    title: "Movie 8",
+  },
+  {
+    id: 9,
+    synopsis: "Quisque eget urna nec purus convallis congue.",
+    image: "https://example.com/image9.jpg",
+    title: "Movie 9",
+  },
+  {
+    id: 10,
+    synopsis: "In vel justo a massa gravida sollicitudin.",
+    image: "https://example.com/image10.jpg",
+    title: "Movie 10",
+  },
+  {
+    id: 11,
+    synopsis: "Vivamus in dolor non elit vulputate lacinia.",
+    image: "https://example.com/image11.jpg",
+    title: "Movie 11",
+  },
+  {
+    id: 12,
+    synopsis: "Sed at odio id augue auctor euismod.",
+    image: "https://example.com/image12.jpg",
+    title: "Movie 12",
+  },
+  {
+    id: 13,
+    synopsis: "Cras ut nisl a libero tristique aliquet.",
+    image: "https://example.com/image13.jpg",
+    title: "Movie 13",
+  },
+  {
+    id: 14,
+    synopsis: "Pellentesque ut sem in erat finibus sodales.",
+    image: "https://example.com/image14.jpg",
+    title: "Movie 14",
+  },
+  {
+    id: 15,
+    synopsis:
+      "Aenean auctor augue vel nisi vehicula, eget auctor mi efficitur.",
+    image: "https://example.com/image15.jpg",
+    title: "Movie 15",
+  },
+  {
+    id: 16,
+    synopsis:
+      "Duis tincidunt augue ac odio posuere, vel scelerisque arcu malesuada.",
+    image: "https://example.com/image16.jpg",
+    title: "Movie 16",
+  },
+  {
+    id: 17,
+    synopsis: "Nunc euismod justo auctor, dignissim urna id, viverra velit.",
+    image: "https://example.com/image17.jpg",
+    title: "Movie 17",
+  },
+  {
+    id: 18,
+    synopsis:
+      "Praesent tincidunt urna in massa efficitur, id hendrerit purus viverra.",
+    image: "https://example.com/image18.jpg",
+    title: "Movie 18",
+  },
+  {
+    id: 19,
+    synopsis: "Ut scelerisque odio a lacinia facilisis.",
+    image: "https://example.com/image19.jpg",
+    title: "Movie 19",
+  },
+  {
+    id: 20,
+    synopsis: "Vestibulum in justo eget lectus tincidunt vestibulum.",
+    image: "https://example.com/image20.jpg",
+    title: "Movie 20",
+  },
+];
---
+++ src/main.ts
@@ -0,0 +1,205 @@
+import { data } from "./data";
+type Movie = (typeof data)[0];
+
+const form: HTMLFormElement = document.getElementById(
+  "form"
+) as HTMLFormElement;
+const clearNominations: HTMLButtonElement = document.getElementById(
+  "clear-nominations"
+) as HTMLButtonElement;
+const saveNominationsButton: HTMLButtonElement = document.getElementById(
+  "save-nominations-button"
+) as HTMLButtonElement;
+
+let foundedMovies: Movie[] = [];
+let nominatedMovies: Movie[] = [];
+
+function onMovieDetail(event: Event, movie: Movie) {
+  event.preventDefault();
+  const modalMovieDetails: HTMLDivElement = document.getElementById(
+    "modal-movie-details"
+  ) as HTMLDivElement;
+  const title: HTMLParagraphElement = document.getElementById(
+    "movie-title"
+  ) as HTMLParagraphElement;
+  const plot: HTMLParagraphElement = document.getElementById(
+    "movie-plot"
+  ) as HTMLParagraphElement;
+  const buttonClose: HTMLButtonElement = document.getElementById(
+    "close-modal"
+  ) as HTMLButtonElement;
+
+  title.innerText = movie.title;
+  plot.innerText = movie.synopsis;
+
+  modalMovieDetails.style.display = "block";
+
+  buttonClose.addEventListener("click", (event) => {
+    event.preventDefault();
+    modalMovieDetails.style.display = "none";
+  });
+}
+
+function clearNominationList() {
+  const ulNominations: HTMLUListElement = document.getElementById(
+    "ul-nominations"
+  ) as HTMLUListElement;
+  const nominations: HTMLDivElement = document.getElementById(
+    "nominations"
+  ) as HTMLDivElement;
+  const saveNominations: HTMLElement = document.getElementById(
+    "save-nominations"
+  ) as HTMLElement;
+
+  while (ulNominations.lastElementChild) {
+    ulNominations.removeChild(ulNominations.lastElementChild);
+  }
+
+  nominations.style.display = "none";
+  saveNominations.style.display = "none";
+}
+
+function clearPreviousSearch() {
+  const ul: HTMLUListElement = document.getElementById(
+    "ul"
+  ) as HTMLUListElement;
+  while (ul.lastElementChild) {
+    ul.removeChild(ul.lastElementChild);
+  }
+}
+
+function onAddMovie(event: Event, movie: Movie) {
+  event.preventDefault();
+
+  if (
+    nominatedMovies?.length === 5 ||
+    nominatedMovies?.findIndex((item) => item.id == movie.id) !== -1
+  ) {
+    return;
+  }
+  nominatedMovies.push(movie);
+
+  const nominations: HTMLDivElement = document.getElementById(
+    "nominations"
+  ) as HTMLDivElement;
+  const saveNominations: HTMLElement = document.getElementById(
+    "save-nominations"
+  ) as HTMLElement;
+
+  saveNominations.style.display = "flex";
+  nominations.style.display = "grid";
+  const ulNominations: HTMLUListElement = document.getElementById(
+    "ul-nominations"
+  ) as HTMLUListElement;
+  const buttonRemove: HTMLButtonElement = document.createElement(
+    "button"
+  ) as HTMLButtonElement;
+
+  buttonRemove.classList.add("icon-button", "secondary");
+  buttonRemove.innerHTML = `<img src="/public/cross-icon.svg" />`;
+
+  const buttonDetails: HTMLButtonElement = document.createElement(
+    "button"
+  ) as HTMLButtonElement;
+
+  buttonDetails.innerHTML = `<p class="paragraph">${movie.title}</p>`;
+
+  const li: HTMLLIElement = document.createElement("li") as HTMLLIElement;
+
+  li.classList.add("list-item");
+
+  li.appendChild(buttonRemove);
+  li.appendChild(buttonDetails);
+
+  ulNominations.appendChild(li);
+
+  buttonRemove.addEventListener("click", (event) => {
+    event.preventDefault();
+    ulNominations.removeChild(li);
+  });
+  buttonDetails.addEventListener("click", (event) =>
+    onMovieDetail(event, movie)
+  );
+}
+
+function showFoundedMovies(searchedValue: string) {
+  const searchResults: HTMLDivElement = document.getElementById(
+    "search-results"
+  ) as HTMLDivElement;
+  const ul: HTMLUListElement = document.getElementById(
+    "ul"
+  ) as HTMLUListElement;
+  const resultsFor: HTMLParagraphElement = document.getElementById(
+    "results-for"
+  ) as HTMLParagraphElement;
+  resultsFor.innerText = `Results for "${searchedValue}"`;
+  searchResults.style.display = "grid";
+
+  foundedMovies.forEach((movie, index) => {
+    const li: HTMLLIElement = document.createElement("li") as HTMLLIElement;
+    li.classList.add("list-item");
+    const buttonAdd: HTMLButtonElement = document.createElement(
+      `button-add-${index}`
+    ) as HTMLButtonElement;
+    const buttonDetails: HTMLButtonElement = document.createElement(
+      `button-details-${index}`
+    ) as HTMLButtonElement;
+
+    const title = document.createElement("p");
+    title.classList.add("paragraph");
+    buttonAdd.classList.add("icon-button", "primary");
+
+    buttonAdd.innerHTML = `<img src="/public/plus-icon.svg" />`;
+    title.innerText = movie.title;
+    buttonDetails.appendChild(title);
+
+    li.appendChild(buttonAdd);
+    li.appendChild(buttonDetails);
+
+    ul.appendChild(li);
+
+    buttonAdd.addEventListener("click", (event) => onAddMovie(event, movie));
+    buttonDetails.addEventListener("click", (event) =>
+      onMovieDetail(event, movie)
+    );
+  });
+}
+
+function search(event: Event) {
+  event.preventDefault();
+
+  let searched = "";
+  for (let i = 0; i < form.length; ++i) {
+    const element: HTMLInputElement = form[i] as HTMLInputElement;
+    if (i === 0) searched = element.value;
+    if (element.type === "text") {
+      const value = element.value;
+      const filteredData = data.filter((movie) => {
+        return movie.title.includes(value);
+      });
+      foundedMovies = filteredData;
+    }
+  }
+  clearPreviousSearch();
+  showFoundedMovies(searched);
+}
+
+form.addEventListener("submit", search);
+clearNominations.addEventListener("click", (event) => {
+  const nominationsBanner: HTMLDivElement = document.getElementById(
+    "all-nominations-banner"
+  ) as HTMLDivElement;
+  event.preventDefault();
+  nominatedMovies = [];
+  clearNominationList();
+  nominationsBanner.style.display = "none";
+});
+
+saveNominationsButton?.addEventListener("click", (event) => {
+  event.preventDefault();
+  const nominationsBanner: HTMLDivElement = document.getElementById(
+    "all-nominations-banner"
+  ) as HTMLDivElement;
+
+  nominationsBanner.style.display = "flex";
+});
